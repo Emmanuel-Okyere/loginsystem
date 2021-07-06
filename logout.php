774b12ed61afdfb505590d41b7f30de17225f4dd
Emmanuel-Okyere <57065066+Emmanuel-Okyere@users.noreply.github.com>
2021-07-06 18:00:30 +0000

    Add files via upload

--- logout.php
+++ logout.php
@@ -6,5 +6,5 @@ session_unset();
 $_SESSION['action1']="You have logged out successfully..!";
 ?>
 <script language="javascript">
-document.location="new.php";
+document.location="index.php";
 </script>
